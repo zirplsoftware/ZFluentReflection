ece261d459db1f3b3ced9592fc140d4591210ac1
Nathan LaFratta <nlafratta@zirpl.com>
2014-12-30 07:28:28 -0500

    oops just noticed there was an extra folder

--- Zirpl.FluentReflection/Zirpl.FluentReflection/Public/IMemberQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace Zirpl.FluentReflection
-{
-    public interface IMemberQuery : INamedMemberQuery<MemberInfo, IMemberQuery>
-    {
-        IMemberTypeSubQuery OfMemberType();
-    }
-}
