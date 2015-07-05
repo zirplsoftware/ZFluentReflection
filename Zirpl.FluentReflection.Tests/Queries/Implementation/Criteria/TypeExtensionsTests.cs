0be1774413083047b7a5abdffe6fb9518675d5bd
Nathan LaFratta <nlafratta@zirpl.com>
2015-07-05 12:44:12 -0400

    added unit tests for TypeExtensions

---
+++ Zirpl.FluentReflection.Tests/Queries/Implementation/Criteria/TypeExtensionsTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Zirpl.FluentReflection.Queries;
+using Zirpl.FluentReflection.Queries.implementation.criteria;
+
+namespace Zirpl.FluentReflection.Tests.Queries.Implementation.Criteria
+{
+    [TestFixture]
+    public class TypeExtensionsTests
+    {
+        [Test]
+        public void TestGetAssociatedType()
+        {
+            MemberInfo memberInfo = null;
+            Type resultingType = null;
+
+            // Event
+            memberInfo = typeof (MockType).GetEvent("InstanceEvent");
+            resultingType = memberInfo.GetAssociatedType(TypeSource.EventHandlerType);
+            resultingType.Should().NotBeNull();
+            resultingType.Should().Be(typeof (EventHandler));
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.FieldType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.MethodReturnType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.PropertyType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.Self)).ShouldThrow<Exception>();
+
+            // Field
+            memberInfo = typeof(MockType).GetField("InstanceField");
+            resultingType = memberInfo.GetAssociatedType(TypeSource.FieldType);
+            resultingType.Should().NotBeNull();
+            resultingType.Should().Be(typeof(Int32));
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.EventHandlerType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.MethodReturnType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.PropertyType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.Self)).ShouldThrow<Exception>();
+
+            // Property
+            memberInfo = typeof(MockType).GetProperty("InstanceProperty");
+            resultingType = memberInfo.GetAssociatedType(TypeSource.PropertyType);
+            resultingType.Should().NotBeNull();
+            resultingType.Should().Be(typeof(Int32));
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.EventHandlerType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.MethodReturnType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.FieldType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.Self)).ShouldThrow<Exception>();
+
+            // Property
+            memberInfo = typeof(MockType).GetMethod("InstanceMethod");
+            resultingType = memberInfo.GetAssociatedType(TypeSource.MethodReturnType);
+            resultingType.Should().NotBeNull();
+            resultingType.Should().Be(typeof(void));
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.EventHandlerType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.PropertyType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.FieldType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.Self)).ShouldThrow<Exception>();
+
+            // Property
+            memberInfo = typeof(MockType).GetNestedType("NestedType");
+            resultingType = memberInfo.GetAssociatedType(TypeSource.Self);
+            resultingType.Should().NotBeNull();
+            resultingType.Should().Be(typeof(MockType.NestedType));
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.EventHandlerType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.PropertyType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.FieldType)).ShouldThrow<Exception>();
+            new Action(() => memberInfo.GetAssociatedType(TypeSource.MethodReturnType)).ShouldThrow<Exception>();
+        }
+
+        #region Helpers
+
+        // instance
+        // static
+        // declared on this type
+        // declared on base type
+
+        protected class MockType
+        {
+            // constructors???
+            // nested types???
+
+            public event EventHandler InstanceEvent;
+
+            public int InstanceField;
+
+            public int InstanceProperty { get; set; }
+
+            public void InstanceMethod()
+            {
+
+            }
+
+            public class NestedType
+            {
+
+            }
+        }
+
+
+        #endregion
+    }
+}
